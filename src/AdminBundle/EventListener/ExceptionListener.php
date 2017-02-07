233567723dc1abed97878113b9cf5f24a4755d24
lulus75 <antoine_mailly@hotmail.com>
2017-02-07 00:19:52 +0000

    Add event listener

---
+++ src/AdminBundle/EventListener/ExceptionListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AdminBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ExceptionListener
+{
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $exception = $event->getException();
+        $message = sprintf(
+            '404: Page Not Found',
+            $exception->getMessage(),
+            $exception->getCode()
+        );
+
+        $response = new Response();
+        $response->setContent($message);
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+        } else {
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        
+        $event->setResponse($response);
+    }
+}
